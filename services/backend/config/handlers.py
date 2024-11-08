85af894f22e5d1687cc9475bcd9eab1fb75c181f
maksimv0202 <maksimv0202@gmail.com>
2024-11-09 03:51:09 +0600

    add: ErrorResponseFactory and custom_exception_handler implementation

--- services/backend/config/handlers.py
+++ services/backend/config/handlers.py
@@ -1,7 +1,9 @@
+from django.http import Http404
 from rest_framework import status
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import (AuthenticationFailed, NotFound,
+                                       PermissionDenied, ValidationError)
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.views import APIView, exception_handler
 
 
 class ExceptionHandler404(APIView):
@@ -39,7 +41,7 @@ class ExceptionHandler400(APIView):
 
 
 class ExceptionHandler403(APIView):
-    exception_message = 'The request contains an invalid authentication data'
+    exception_message = 'You do not have permission to access this resource'
 
     def get(self, request, *args, **kwargs):
         return Response({'error': self.exception_message},
@@ -54,17 +56,53 @@ class ErrorResponseFactory:
     """
     Factory class to generate custom JSON responses for different exceptions.
 
-    This approach follows the Open/Closed Principle, allowing easy
-    extension for new exception types.
+    This class uses Python's match/case statements to determine the appropriate error message,
+    status code, and error details based on the exception type. It enables consistent and
+    structured JSON responses for various common exceptions, such as validation errors,
+    authentication errors, permission denials, and resource not found errors.
     """
 
     @staticmethod
     def get_error_response(exception, response=None):
-        pass
+        match exception:
+            case ValidationError():
+                return ErrorResponseFactory._build_response(
+                    status_code=status.HTTP_400_BAD_REQUEST,
+                    error='Validation Error',
+                    message=response.data if response else str(exception),
+                )
+            case NotFound() | Http404():
+                return ErrorResponseFactory._build_response(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    error='Not Found',
+                    message='The requested resource was not found'
+                )
+            case PermissionDenied():
+                return ErrorResponseFactory._build_response(
+                    status_code=status.HTTP_403_FORBIDDEN,
+                    error='Permission Denied',
+                    message='You do not have permission to access this resource'
+                )
+            case AuthenticationFailed():
+                return ErrorResponseFactory._build_response(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    error='Authentication Failed',
+                    message='Authentication credentials were not provided or are invalid'
+                )
+            case _:
+                return ErrorResponseFactory._build_response(
+                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    error='Internal Server Error',
+                    message='An unexpected error occurred on the server'
+                )
 
     @staticmethod
-    def _build_response(message, error, status_code):
-        pass
+    def _build_response(status_code, error, message):
+        return {
+            'status_code': status_code,
+            'error': error,
+            'message': message
+        }, status_code
 
 
 def custom_exception_handler(exc, context):
@@ -78,4 +116,14 @@ def custom_exception_handler(exc, context):
     and 'message'. Unhandled exceptions will return a generic 500 error
     response with a corresponding message.
     """
-    pass
+    response = exception_handler(exc, context)
+
+    if not response:
+        custom_response_data, status_code = ErrorResponseFactory.get_error_response(exc)
+        return Response(custom_response_data, status=status_code)
+
+    custom_response_data, status_code = ErrorResponseFactory.get_error_response(exc, response)
+    response.data = custom_response_data
+    response.status_code = status_code
+
+    return response
